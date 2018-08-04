164be12fc1bf92bebc3e7702921579243a88372e
John Kulimushi <klsjohn2@gmail.com>
2018-08-04 12:11:14 +0100

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter basename={"~/apps/blog-demo-app"}>
+    <BrowserRouter>
         <App />
     </BrowserRouter>
     , document.getElementById('root'));
